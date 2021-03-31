edaab4df43db15b2b0903ef28c05af6d2791db73
Gabor Imre <imregabor@gmail.com>
2021-03-31 11:45:30 +0200

    Move text conversion to a function

--- nmr-result-process/src/process-nmr-result-to-text.py
+++ nmr-result-process/src/process-nmr-result-to-text.py
@@ -16,30 +16,41 @@ def get_attached_hydrogens(c_atom_index, hnmr_result):
         if (hnmr["attachedAtomIndex"] == c_atom_index): hydrogens.append(hnmr["atomIndex"])
     return hydrogens
 
+
+# Convert response to a textual representation
+def response_to_text(data):
+    ret = []
+
+    # https://stackoverflow.com/a/522578
+    for index, d in enumerate(data["results"]):
+
+        ret.append("Structure #{}".format(index))
+        ret.append("    13C NMR result:")
+        ret.append("    Atom      Shift         Hydrogens")
+
+        cshifts = []
+        for r in d["cnmrResult"]:
+            cshifts.append(r["shift"])
+            ret.append("    {:2}        {:6.2f}        {}".format(r["atomIndex"], r["shift"], get_attached_hydrogens(r["atomIndex"], d["hnmrResult"])))
+
+        ret.append("")
+
+        ret.append("    1H NMR result:")
+        ret.append("    Atom      Shift")
+        hshifts = []
+        for r in d["hnmrResult"]:
+            hshifts.append(r["shift"])
+            ret.append("    {:2}        {:6.2f}".format(r["atomIndex"], r["shift"]))
+
+
+        ret.append("");
+    
+    
+    return '\n'.join(ret)
+
+
 # parse json input from stdin
 data = json.load(sys.stdin)
 
-# https://stackoverflow.com/a/522578
-for index, d in enumerate(data["results"]):
-
-    print("Structure #{}".format(index))
-    print("    13C NMR result:")
-    print("    Atom      Shift         Hydrogens")
-
-    cshifts = []
-    for r in d["cnmrResult"]:
-        cshifts.append(r["shift"])
-        print("    {:2}        {:6.2f}        {}".format(r["atomIndex"], r["shift"], get_attached_hydrogens(r["atomIndex"], d["hnmrResult"])))
-
-    print("")
-
-    print("    1H NMR result:")
-    print("    Atom      Shift")
-    hshifts = []
-    for r in d["hnmrResult"]:
-        hshifts.append(r["shift"])
-        print("    {:2}        {:6.2f}".format(r["atomIndex"], r["shift"]))
-
-
-    print("");
+print(response_to_text(data))
 
